c42632be04b488f382088e135d5b0178e2e27494
Andrew Zhu <me@andrew-zhu.com>
2020-07-09 15:57:49 -0700

    add auth endpoint to exchange code grant for jwt

--- blueprints/discord.py
+++ blueprints/discord.py
@@ -1,11 +1,64 @@
-from flask import Blueprint
+import datetime
 
-from lib.discord import UserInfo, fetch_user_info, get_user_info
-from lib.utils import jsonify
+import jwt
+from flask import Blueprint, request
+from requests import HTTPError
+
+import config
+from lib.discord import exchange_code, fetch_user_info, handle_token_response
+from lib.utils import error, jsonify, success
 
 discord = Blueprint('discord', __name__)
 
 
 @discord.route("users/<user_id>", methods=["GET"])
 def get_user(user_id):
+    """
+    GET /discord/users/:id
+
+    Returns:
+    (UserInfo)
+    """
     return jsonify(fetch_user_info(user_id).to_dict())
+
+
+@discord.route("auth", methods=["POST"])
+def handle_auth():
+    """
+    POST /discord/auth
+    Content-Type: application/json
+
+    {"code": str}
+
+    Returns:
+    {"success": bool, "data": {"jwt": str}, "error": str}
+    """
+    data = request.json
+    if data is None:
+        return error(400, "missing body")
+    if 'code' not in data:
+        return error(400, f"code is required")
+    if not isinstance(data['code'], str):
+        return error(400, f"code must be str")
+
+    try:
+        access_token_resp = exchange_code(data['code'])
+        _, user = handle_token_response(access_token_resp)
+    except HTTPError as e:
+        if 400 <= e.response.status_code < 500:
+            return error(e.response.status_code, str(e))
+        return error(500, str(e))
+
+    token = jwt.encode(
+        {
+            'iss': 'avrae.io',
+            'aud': 'avrae.io',
+            'iat': datetime.datetime.now(),
+            'id': str(user.id),
+            'username': user.username,
+            'discriminator': user.discriminator,
+            'avatar': user.avatar
+        },
+        config.JWT_SECRET, algorithm='HS256')
+
+    return success({'jwt': token.decode()})  # token is str
--- lib/discord.py
+++ lib/discord.py
@@ -1,19 +1,98 @@
+import datetime
+
 import requests
 from flask import abort, current_app, request
 
-DISCORD_API = "https://discordapp.com/api/v6"
+import config
+
+DISCORD_API = "https://discord.com/api/v6"
 DISCORD_CDN = "https://cdn.discordapp.com"
 HEADERS = {
     "User-Agent": "DiscordBot (https://github.com/avrae/avrae.io, 1)"
 }
 
 
+# oauth
+def exchange_code(code):
+    data = {
+        'client_id': config.DISCORD_CLIENT_ID,
+        'client_secret': config.DISCORD_CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': config.OAUTH_REDIRECT_URI,
+        'scope': config.OAUTH_SCOPE
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post(f'{DISCORD_API}/oauth2/token', data=data, headers=headers)
+    r.raise_for_status()
+    return r.json()
+
+
+def refresh_token(ref_token):
+    data = {
+        'client_id': config.DISCORD_CLIENT_ID,
+        'client_secret': config.DISCORD_CLIENT_SECRET,
+        'grant_type': 'refresh_token',
+        'refresh_token': ref_token,
+        'redirect_uri': config.OAUTH_REDIRECT_URI,
+        'scope': config.OAUTH_SCOPE
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post(f'{DISCORD_API}/oauth2/token', data=data, headers=headers)
+    r.raise_for_status()
+    return r.json()
+
+
+def handle_token_response(access_token_resp):
+    access_token = access_token_resp['access_token']
+    ref_token = access_token_resp['refresh_token']
+    expiry = datetime.datetime.now() + datetime.timedelta(seconds=access_token_resp['expires_in'])
+
+    r = get("/users/@me", access_token)
+    r.raise_for_status()
+
+    user = r.json()
+    user['discord_auth'] = {"access_token": access_token, "expiry": expiry, "refresh_token": ref_token}
+
+    # store user access token
+    current_app.mdb.users.update_one(
+        {"id": str(user['id'])},
+        {"$set": user},
+        upsert=True
+    )
+
+    # return current access token and user info
+    return access_token, UserInfo(user)
+
+
+def discord_token_for(user_id: str):
+    """Gets the current discord access token for the given user id, refreshing if necessary."""
+    user = current_app.mdb.users.find_one({"id": user_id})
+    if user is None:
+        return None
+    if 'discord_auth' not in user:
+        return None
+
+    expiry = user['discord_auth']['expiry']
+    if expiry < datetime.datetime.now():
+        resp = refresh_token(user['discord_auth']['refresh_token'])
+        token, _ = handle_token_response(resp)
+        return token
+    else:
+        return user['discord_auth']['access_token']
+
+
+# user
 class UserInfo:
     def __init__(self, user):
-        self.username = user['username']
-        self.id = user['id']
-        self.discriminator = user['discriminator']
-        self.avatar = user['avatar']
+        self.username = user['username']  # type: str
+        self.id = user['id']  # type: str
+        self.discriminator = user['discriminator']  # type: str
+        self.avatar = user['avatar']  # type: str or None
 
     def get_avatar_url(self):
         if self.avatar:
